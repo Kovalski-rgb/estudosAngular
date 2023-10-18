8ea0e08cbe4f90ae2bef64741b0e78dd1ef1f4c1
Kovalski-rgb <andre.kovalski@tecpuc.com.br>
2023-10-18 13:41:33 -0300

    tarefa 1 concluida

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './assignment/warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './assignment/success-alert/success-alert.component';
 
 @NgModule({
   // declara que tal componente existe
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule
@@ -19,4 +23,3 @@ import { ServersComponent } from './servers/servers.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
