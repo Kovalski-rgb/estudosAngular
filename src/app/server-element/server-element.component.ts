907ef597d0a7e685d2337d6c0a5883277955e6c2
kovalski-rgb <andre.kovalski@tecpuc.com.br>
2023-10-23 13:37:05 -0300

    event binding alias e view encapsulation

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,9 +1,14 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  // view encapsulation é basicamente o ""container"" 
+  // de cada css, geralmente é tudo por ShadowDom, 
+  // ou seja, se eu pedir pra alterar o body no css
+  // desse componente, somente o body do componente é alterado, nada mais
+  encapsulation: ViewEncapsulation.Emulated //None, ShadowDom
 })
 export class ServerElementComponent implements OnInit {
   @Input('srvElement')
