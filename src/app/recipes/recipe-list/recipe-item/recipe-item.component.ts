1df213ccd753fd400e9207b8ead415b466b86a3c
kovalski-rgb <andre.kovalski@tecpuc.com.br>
2023-10-24 14:35:40 -0300

    minha implementação seleção de receitas

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Recipe } from '../../recipe.model';
 
 @Component({
@@ -7,10 +7,15 @@ import { Recipe } from '../../recipe.model';
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
-  @Input() recipe: Recipe;
+  @Input('recipeData') recipe: Recipe;
+  @Output('recipeSelected') recipeClicked = new EventEmitter<Recipe>(); 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  recipeSelected(){
+    this.recipeClicked.emit(this.recipe);
   }
 
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,14 +7,19 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output('recipeSelected') recipeClicked = new EventEmitter<Recipe>(); 
   recipes: Recipe[] = [
     new Recipe("a Test Recipe", "This is simply a test", "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg"),
-    new Recipe("a Test Recipe", "This is simply a test", "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg"),
+    new Recipe("another Test Recipe", "This is simply another test", "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg"),
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  recipeSelected(recipe : Recipe){
+    this.recipeClicked.emit(recipe);
   }
 
 }
