6096733ca22455cd1703ea1fea8e3f19186a41b1
kovalski-rgb <andre.kovalski@tecpuc.com.br>
2023-10-27 10:11:09 -0300

    hostListeners

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
@@ -12,7 +12,19 @@ export class BetterHighlightDirective implements OnInit {
   }
 
   ngOnInit(): void {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+  }
+
+  @HostListener('mouseenter') mouseOver(eventData : Event) {
     this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+  }
+
+  @HostListener('mouseleave') mouseLeave(eventData : Event) {
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+  }
+
+  @HostListener('click') mouseClick(eventData : Event) {
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'red');
   }
 
 }
