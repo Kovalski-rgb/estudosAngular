ca882b603e8c151609fb3290b4675abeb857ec26
kovalski-rgb <andre.kovalski@tecpuc.com.br>
2023-11-06 09:20:45 -0300

    criação dos arquivos de servico, upload projeto

--- src/app/shared/dropdown.directive-instructor.ts
+++ src/app/shared/dropdown.directive-instructor.ts
@@ -11,10 +11,6 @@ export class DropdownInsDirective {
 
   }
   @HostBinding('class.open') isOpen = false;  
-  // @HostListener('click') onClick(){
-  //   this.isOpen = !this.isOpen;
-  // } 
-
   @HostListener ('document:click', ['$event']) toggleOpen(event: Event){
     this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
   }
