b41081a91ad4883022534bc8c6e99de8dd23017e
kovalski-rgb <andre.kovalski@tecpuc.com.br>
2023-11-06 10:37:23 -0300

    trocando event binding por servicos para comm entre serviços

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -11,9 +11,14 @@ import { RecipeService } from './recipes.service';
 export class RecipesComponent implements OnInit {
   selectedRecipe: Recipe;
 
-  constructor() { }
+  constructor(private recipeService : RecipeService) { }
 
   ngOnInit(): void {
+    this.recipeService.recipeSelected.subscribe(
+      (recipe : Recipe) => {
+        this.selectedRecipe = recipe;
+      }
+    );
   }
 
   recipeSelected(recipe: Recipe){
