bafab5df3c7945fceb9c353eb4baee17057f4d78
Kovalski <andre.kovalski@tecpuc.com.br>
2023-11-16 11:13:46 -0300

    fake auth e routeGuard para child routes

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from "@angular/router";
+import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
 import { Observable } from "rxjs";
 import { AuthService } from "./auth.service";
 
 @Injectable()
-export class AuthGuard {
+export class AuthGuard implements CanActivate, CanActivateChild{
 
     constructor(private authService: AuthService, private router: Router){
 
+    }
+    canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
+        return this.canActivate(childRoute, state);
     }
 
     canActivate(route: ActivatedRouteSnapshot,
